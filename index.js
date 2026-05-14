712d795baafa6c92b149ac714d0975e795cf05f4
Arbazali1748 <146565110+Arbazali1748@users.noreply.github.com>
2026-05-14 18:02:33 +0500

    Fix vercel related deployment Isssue.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import Router from "./Routes/index.js";
+
 const app = express();
 const Port = process.env.Port || 3000;
+
 app.use(express.json());
+
 app.get('/', (req, resp) => {
     return resp.send("Hi Everyone.");
-})
+});
 
 // Route file Setting Here
 app.use("/api", Router);
@@ -16,5 +19,4 @@ if (process.env.NODE_ENV !== "production") {
     app.listen(Port, () => console.log(`server is running on port ${Port}`));
 }
 
-//app.listen(Port, () => console.log(`server is  running on port ${Port}`))
 export default app;
