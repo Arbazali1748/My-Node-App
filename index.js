f4f6f1ae5bfc2df0e90e959529332ef04d92a31e
Arbazali1748 <146565110+Arbazali1748@users.noreply.github.com>
2026-05-14 20:35:20 +0500

    fix prisma duplicate Issue...

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import { fileURLToPath } from "url";
 import Router from "./Routes/index.js";
 
 const app = express();
-const PORT = process.env.Port || process.env.PORT || 3000;
-
-const __filename = fileURLToPath(import.meta.url);
 
 app.use(express.json());
 
@@ -15,11 +12,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", Router);
-
-if (process.argv[1] === __filename) {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
 
 export default app;
