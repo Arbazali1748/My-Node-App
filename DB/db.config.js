413b9a425ecae72de8c79d2cbef6fd3e105d1ecb
Arbazali1748 <146565110+Arbazali1748@users.noreply.github.com>
2026-05-14 17:40:27 +0500

    Database move to neon

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,18 +1,44 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
 
+// for local development, we can use localhost
+
+// import pkg from "@prisma/client";
+// const { PrismaClient } = pkg;
+
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { Pool } from "pg";
+
+// const connectionString = process.env.DATABASE_URL;
+
+// const pool = new Pool({ connectionString });
+
+// const adapter = new PrismaPg(pool);
+
+// const prisma = new PrismaClient({
+//     adapter,
+//     log: ['query'],
+// });
+
+// export default prisma;
+
+// for neon, we need to use the connection string provided by Neon
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
-    adapter,
-    log: ['query'],
+  adapter,
+  log: ["query"],
 });
 
 export default prisma;
