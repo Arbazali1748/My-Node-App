f5f4d250956015aa41b5df352ca48191e1ea0c78
Arbazali1748 <146565110+Arbazali1748@users.noreply.github.com>
2026-05-14 20:23:04 +0500

    fix prisma duplicate Issue..

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prisma = new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+    }),
 });
 
 console.log("Prisma connected");
